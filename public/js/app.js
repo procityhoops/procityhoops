84c51d8ab3ffed022c5e699bc528cae4f91f69f3
bode4540@gmail.com <lucas_09>
2017-02-19 18:13:56 -0600

    Implemented node.js

---
+++ public/js/app.js
@@ -0,0 +1,1 @@
+angular.module('proCityHoopsApp', ['ngRoute', 'appRoutes']);
---
+++ public/js/appRoutes.js
@@ -0,0 +1,26 @@
+angular.module('appRoutes', []).config(function($routeProvider) {
+
+	$routeProvider
+
+		// route for the home page
+	.when('/', {
+		templateUrl : 'home.html'
+	})
+
+	.when('/toronto-raptors', {
+		templateUrl : 'teams/toronto-raptors.html'
+	})
+
+	.when('/indiana-pacers', {
+		templateUrl : 'teams/indiana-pacers.html'
+	})
+
+	.when('/cleveland-cavaliers', {
+		templateUrl : 'teams/cleveland-cavaliers.html'
+	})
+
+	.when('/fantasy-football', {
+		templateUrl : 'articles/Article1.html'
+	})
+
+});
