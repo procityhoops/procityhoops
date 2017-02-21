a721574126ebe46ebe9cd76a2db2a650dc64d959
bode4540@gmail.com <lucas_09>
2017-02-20 21:05:22 -0600

    Added NBA API

--- app/routes.js
+++ app/routes.js
@@ -1,8 +1,17 @@
 module.exports = function(app) {
-
+	const nba = require('nba.js').default;
 	// server routes ===========================================================
 	// handle things like api calls
 	// authentication routes
+	app.get('/api/teams', function (req, response) {
+		nba.data.teams({
+			year: 2016
+		}).then(function(res) {
+			response.send(res)
+		}).catch(function(err) {
+			console.error(err);
+		});
+	})
 
 	// frontend routes =========================================================
 	// route to handle all angular requests
