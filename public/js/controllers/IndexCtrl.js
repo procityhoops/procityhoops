23a63a827e40ca0edb9e16191cb7e148a4faa346
bode4540@gmail.com <lucas_09>
2017-05-06 13:15:26 -0500

    Updated google analytics to work in angular app.

--- public/js/controllers/IndexCtrl.js
+++ public/js/controllers/IndexCtrl.js
@@ -1,7 +1,13 @@
-angular.module('IndexCtrl', []).controller('IndexController', function($scope, $http) {
+angular.module('IndexCtrl', []).controller('IndexController', function($scope, $http, $window, $location) {
 
 	$http.get("/api/team")
 		.then(function(response){ 
 			$scope.teams = response.data;
+	});
+
+	$scope.$on('$viewContentLoaded', function(event) {
+		if (document.location.hostname.search("procityhoops.com") !== -1) {
+			$window.ga('send', 'pageview', {page: $location.url()});
+		}
 	});
 });
