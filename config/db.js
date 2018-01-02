7d4e7239170069ad0672f1a55a3822d403a70294
bode4540@gmail.com <lucas_09>
2018-01-01 20:04:53 -0500

    Removed from config.

--- config/db.js
+++ config/db.js
@@ -1,4 +1,3 @@
 module.exports = {
 	url: process.env.dbURL || 'mongodb://localhost:27017'
-	//url: process.env.dbURL || 'mongodb://procityhoops-prod:$K3vUCZA3XHbLPUC@ds119151.mlab.com:19151/procityhoops'
 }
